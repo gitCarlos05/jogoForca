fc19ce00a8855e156b0b6ea40aadf2b57157abb2
Carlos Araújo <valdircarlos2005@gmail.com>
2023-06-25 19:53:08 -0300

    finalizado

--- forca.js
+++ forca.js
@@ -1,8 +1,8 @@
 const palavrasForca = ['BRASIL', 'NORUEGA', 'AUSTRIA', 'FRANÇA', 'ARGENTINA', 'URUGUAY', 'CHILE', 'MOÇAMBIQUE', 'CHINA', 'PERU'];
 
-
 var vidas = 6;
 var letrasCorretas = [];
+var letrasIncorretas = [];
 var palavraAleatoria = Math.floor(Math.random() * palavrasForca.length);
 var palavra = palavrasForca[palavraAleatoria];
 
@@ -26,15 +26,41 @@ document.addEventListener("keyup", function (event) {
 
                 }
             }
-        }else{
-        document.getElementById("letras-incorretas").innerHTML += letraDigitada+",";
+        } else {
+            if (!letrasIncorretas.includes(letraDigitada)) {
+                letrasIncorretas.push(letraDigitada);
+                document.getElementById("letras-incorretas").innerHTML += letraDigitada + ",";
+                vidas--;
+
+                document.getElementById("quantidade-vidas").innerHTML = vidas;
+
+                if (vidas === 0) {
+                    alert("Fim de jogo!! Pressione ok para iniciar novamente.");
+                    reiniciarJogo();
+                }
+            }
         }
-    } else {
-        console.log(event);
-    }
-});
+        } else {
+            alert("Somente letras são permitidas!!");
+        }
+    });
 
 function mostraDica() {
     alert("A resposta é ' " + palavra + " '")
-    console.log(palavra);
 };
+
+function reiniciarJogo() {
+    // Lógica para reiniciar o jogo
+    letrasCorretas = [];
+    vidas = 6;
+    palavraAleatoria = Math.floor(Math.random() * palavrasForca.length);
+    palavra = palavrasForca[palavraAleatoria];
+
+    document.getElementById("palavra-escolhida").innerHTML = "";
+    for (var i = 0; i < palavra.length; i++) {
+        document.getElementById("palavra-escolhida").innerHTML += `<span style="margin-left:15px;" class="underscore underscore${i}">_</span>`;
+    }
+
+    document.getElementById("letras-incorretas").innerHTML = "";
+    document.getElementById("quantidade-vidas").innerHTML = vidas;
+}
